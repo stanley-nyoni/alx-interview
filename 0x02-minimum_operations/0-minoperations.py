b3748a95344c48473057d275c813533a708e70c9
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2024-08-23 22:19:54 +0200

    min operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 '''The minimum operations coding challenge.'''
+
+
 def minOperations(n):
     '''Calculates the fewest number of operations needed to result.'''
     if not isinstance(n, int):
@@ -7,20 +9,15 @@ def minOperations(n):
     ops_count = 0
     clipboard = 0
     done = 1
-   
     while done < n:
         if clipboard == 0:
-            
             clipboard = done
             done += clipboard
             ops_count += 2
-          
         elif n - done > 0 and (n - done) % done == 0:
-            
             clipboard = done
             done += clipboard
             ops_count += 2
-            
         elif clipboard > 0:
             # paste
             done += clipboard
