e26c736dfe8c57b3526717bc52375ca00fb191a3
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2024-09-19 22:23:19 +0200

    long parse

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,47 +1,50 @@
 #!/usr/bin/python3
-'''A script for parsing HTTP request logs with status codes'''
+"""
+log parsing
+"""
 
 import sys
-import signal
+import re
 
-total_size = 0
-status_codes_count = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
-line_count = 0
 
-def print_stats():
-    print(f"File size: {total_size}")
-    for code in sorted(status_codes_count.keys()):
-        if status_codes_count[code] > 0:
-            print(f"{code}: {status_codes_count[code]}")
+def output(log: dict) -> None:
+    """
+    helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
-def signal_handler(sig, frame):
-    print_stats()
-    sys.exit(0)
 
-signal.signal(signal.SIGINT, signal_handler)
+if __name__ == "__main__":
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
 
-for line in sys.stdin:
-    parts = line.split()
-    if len(parts) != 10:
-        continue
-    ip, dash, date, method, url, protocol, status_code, file_size = parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], parts[9]
-    
+    line_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
+
     try:
-        total_size += int(file_size)
-        if status_code in status_codes_count:
-            status_codes_count[status_code] += 1
-    except ValueError:
-        continue
-    
-    line_count += 1
-    if line_count % 10 == 0:
-        print_stats()
+        for line in sys.stdin:
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if (match):
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
+
+                # File size
+                log["file_size"] += file_size
+
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
+
+                if (line_count % 10 == 0):
+                    output(log)
+    finally:
+        output(log)
